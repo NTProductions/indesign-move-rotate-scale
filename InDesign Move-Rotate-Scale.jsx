60bc9533b2d2c4499935c7d43a6f30120b72d3a2
Nate Lovell <Natelovell99@gmail.com>
2022-05-14 23:56:05 -0600

    Create InDesign Move-Rotate-Scale.jsx

---
+++ InDesign Move-Rotate-Scale.jsx
@@ -0,0 +1,9 @@
+﻿var layer = app.activeDocument.activeLayer;
+var rects = layer.rectangles;
+
+var rect = rects[0];
+
+rect.move([500, 500]);
+rect.horizontalScale*=2;
+rect.verticalScale*=2;
+rect.rotationAngle=45;
